fef6b9e8d2cdacc2fc9f59eeb36d06e91efe1887
TyWalter <walterty89@gmail.com>
2024-05-04 00:50:43 -0500

    Got the toggleable light/dark mode working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,31 +2,36 @@ const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
 
 function getIcon(){
-  const mode = localStorage.getItem("icon")
-  if(mode === sun){
-    
+  const mode = localStorage.getItem("icon");
+  if(mode === "sun"){
+    displayMoon();
+  } else if(mode === "moon"){
+    displaySun();
   }
 };
 
 function saveToStorage(e){
-  e.preventDefault()
+  e.preventDefault();
   if(e.target.id === "sun"){
-    localStorage.setItem("icon", "sun") 
+    localStorage.setItem("icon", "sun"); 
+    getIcon();
   } else if(e.target.id === "moon"){
-    localStorage.setItem("icon", "moon")
+    localStorage.setItem("icon", "moon");
+    getIcon();
   }
-  changeIcon()
 };
 
-function changeIcon(){
-  if(sun.style.display === "inline"){
-    sun.style.display = "none"
-    moon.style.display = "inline"
-  } else if(sun.style.display === "none"){
-    moon.style.display = "none"
-    sun.style.display = "inline"
-  }
-};
+function displaySun(){
+  sun.style.display = "inline";
+  moon.style.display = "none";
+}
+
+function displayMoon(){
+  sun.style.display = "none";
+  moon.style.display = "inline";
+}
 
 sun.addEventListener("click", saveToStorage);
 moon.addEventListener("click", saveToStorage);
+
+getIcon();
